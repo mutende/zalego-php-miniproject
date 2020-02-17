5da287960ab3a4407583bcd6526dee0d8b196ab3
mutende <35865219+mutende@users.noreply.github.com>
2020-02-17 18:40:33 +0300

    Update dbconnection.php

--- files/dbconnection.php
+++ files/dbconnection.php
@@ -1,9 +1,9 @@
 <?php
 
-define('DB_HOST', 'localhost');
-define('DB_USER', 'mutende');
-define('DB_PASS', 'm2825');
-define('DB_NAME', 'interview');
+define('DB_HOST', 'server');
+define('DB_USER', 'user');
+define('DB_PASS', 'pass');
+define('DB_NAME', 'db');
 
 $dsn = 'mysql:host='.DB_HOST.';dbname='.DB_NAME;
 $pdo = new PDO($dsn,DB_USER,DB_PASS);
